039b291158a1279013c5001962e81d3a70179b49
Mariia Madarash <mariiamadarash@gmail.com>
2026-05-13 19:09:07 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-// write your code here
+const countries = document.querySelectorAll('.population');
+let total = 0;
+
+for (const country of countries) {
+  const cleaned = country.textContent.replaceAll(',', '');
+  const number = Number(cleaned);
+
+  total += number;
+}
+
+const average = total / countries.length;
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = total.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
